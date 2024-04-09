91f821ea2b84296a33d09fe9e562a86a122390ee
Martin Bassett <101489082+mbassett3@users.noreply.github.com>
2024-04-09 10:57:09 -0400

    Update teamMartinRyan.c with comments

--- teamMartinRyan.c
+++ teamMartinRyan.c
@@ -5,29 +5,32 @@ void shake();
 
 int main()
 {
+    //this enables the servos and the code to connect to the roomba
     create_connect();
     enable_servos();
 
+    //this sets the distance and total angle to 0 every time the code runs
     set_create_distance(0);
     set_create_total_angle(0);
 
-    start();
-    msleep(1000);
+    start(); // start program
+    msleep(1000); // pause for a second
 
-    while (get_create_distance() < 593)
+    while (get_create_distance() < 593) // move to drop
     {
         create_drive_direct(200,200);
     }
     create_stop();
 
-    msleep(5000);
+    msleep(5000); // pause for 5 seconds
 
-    shake();
+    shake(); // shake to make sure noodles are properly in place
 
-    while (get_create_lbump() == 0 && get_create_rbump() == 0){
+    while (get_create_lbump() == 0 && get_create_rbump() == 0){ // move to lava tube zone
         create_drive_direct(100,100);
     }
 
+    // lower arm
     int x;
     for(x = 850; x <= 1200; x++)
     {
@@ -35,15 +38,18 @@ int main()
         msleep(5);
     }
 
+    // drop first noodle
     set_servo_position(1,2047);
     shake();
     set_servo_position(1,0);
 
     create_drive_direct(-100,100);
     msleep(100);
+	//this turns to the one next to the tube in the corner
     
     set_servo_position(1,2047);
     shake();
+    //this allows it so that it properly drops it into the tube
 
     create_stop();
     create_disconnect();
@@ -60,6 +66,7 @@ void start()
         msleep(2);
     }
 }
+//this function sets the servo starting positions
 
 void shake(){ 
     int x;
@@ -72,3 +79,4 @@ void shake(){
         msleep(50);
     }
 }
+//this function makes it so that the robot shakes so that the purple pool noodle goes in
